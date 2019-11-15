1a4a0ab043e450ba5bd4d53a3f0ec58124292319
dan make <danmakes2022@gmail.com>
2019-11-15 11:37:39 -0500

    Update chat-server.ts

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -1,24 +1,27 @@
 import * as express from 'express';
-import * as socketIo from 'socket.io'; 
-import { createServer, Server } from 'http'; 
+import { createServer, Server } from 'http';
+import * as socketIo from 'socket.io'; // new
 
 export class ChatServer {
 
     public static readonly PORT:number = 5000;
     private app: express.Application;
     private port: string | number;
-    private server: Server; 
+    private server: Server;
+    private io: SocketIO.Server;
+    private socketsArray = [];
 
     constructor() {
         this.createApp();
         this.config();
-        this.createServer(); 
-	this.sockets();
+        this.createServer();
+        this.sockets();
         this.listen();
     }
 
     private createApp(): void {
         this.app = express();
+        this.app.use(express.static('public'));
     }
 
     private config(): void {
@@ -26,50 +29,47 @@ export class ChatServer {
     }
 
     private listen(): void {
-	    this.io.on('connection', (socket) => {
+        this.server.listen(this.port, () => {
+            console.log('Running server on port %s', this.port);
+        });
+
+        this.io.on('connection', (socket) => {
             socket.broadcast.emit('add-users', {
                 users: [socket.id]
             });
-	socket.on('disconnect', () => {
+
+            socket.on('disconnect', () => {
                 this.socketsArray.splice(this.socketsArray.indexOf(socket.id), 1);
                 this.io.emit('remove-user', socket.id);
             });
-		   
+
+            socket.on('make-offer', (data) => {
+                socket.to(data.to).emit('offer-made', {
+                    offer: data.offer,
+                    socket: socket.id
+                });
+            });
+
+            socket.on('make-answer', (data) => {
+                socket.to(data.to).emit('answer-made', {
+                    socket: socket.id,
+                    answer: data.answer
+                });
+            });
+
         });
     }
-	
 
     private createServer(): void {
         this.server = createServer(this.app);
     }
 
+     
+    private sockets(): void {
+        this.io = socketIo(this.server);
+    }
+
     public getApp(): express.Application {
         return this.app;
     }
-    private createApp(): void {
-  	this.app = express();
-	this.app.use(express.static('public'));
-    }
-	private sockets(): void {
- 	this.io = socketIo(this.server);
-	}
-	
-	export class ChatServer {
-        private socketsArray = [];
-	
-	}
-
-	socket.on('make-offer', function (data) {
-  	socket.to(data.to).emit('offer-made', {
-    	offer: data.offer,
-    	socket: socket.id
-    });
-});
-
-socket.on('make-answer', function (data) {
-  socket.to(data.to).emit('answer-made', {
-    socket: socket.id,
-    answer: data.answer
-  });
-});
 }
